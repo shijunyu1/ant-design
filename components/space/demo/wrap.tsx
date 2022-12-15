70afd589b20f827d46d6a81df3c9dca4f8a9d455
shijunyu1 <66319709+shijunyu1@users.noreply.github.com>
2022-12-15 11:38:08 +0800

    Update wrap.tsx

--- components/space/demo/wrap.tsx
+++ components/space/demo/wrap.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button, Space } from 'antd';
 
 const App: React.FC = () => (
-  <Space size={[8, 16]} wrap>
+  <Space size={[8, 24]} wrap>
     {new Array(20).fill(null).map((_, index) => (
       // eslint-disable-next-line react/no-array-index-key
       <Button key={index}>Button</Button>
